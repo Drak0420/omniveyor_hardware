dc0794363b01dbd7d9cc549a69e5935f839d91a5
Jason <aasdf34q5sdafg456tw4qert@gmail.com>
2024-05-24 12:34:23 -0400

    Updated teleop scripts

--- pcv_base/scripts/teleop_twist_joystick.py
+++ pcv_base/scripts/teleop_twist_joystick.py
@@ -6,7 +6,8 @@ from std_msgs.msg import Byte
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
-class joystickTeleop():
+
+class joystickTeleop:
     def __init__(self):
         self.msg = """
 Reading from the controller and publishing commands!
@@ -25,9 +26,9 @@ OPTION: Enable/Disable motors
 Circle: Enable/Disable rotation with left joystick
 Cross: Help Menu
 """
-        rospy.Subscriber('joy',Joy, self.joy_cb)
-        self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
-        self.ena_pub = rospy.Publisher('control_mode', Byte, queue_size = 1)
+        rospy.Subscriber("joy", Joy, self.joy_cb)
+        self.vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.ena_pub = rospy.Publisher("control_mode", Byte, queue_size=1)
         self.speed = rospy.get_param("~speed", 0.5)
         self.turn = rospy.get_param("~turn", 1.0)
         self.ena = False
@@ -39,6 +40,7 @@ Cross: Help Menu
         self.scaling_factor = {-1.0: 0.9, 1.0: 1.1}
         rospy.on_shutdown(self.shutdown)
         # hold until subscribers ready
+        """
         r = rospy.Rate(30.0)
         while not self.ena_pub.get_num_connections():
             if not rospy.is_shutdown():
@@ -47,14 +49,31 @@ Cross: Help Menu
                 print(self.msg)
                 self.base_disable()
                 return
+        """
 
     def joy_cb(self, msg):
         # map msg to user_readable values
-        ljoy_hort, ljoy_vert, l2, rjoy_hort, rjoy_vert, r2, dpad_hort, dpad_vert = msg.axes
-        cross, circle, triangle, square, l1, r1, l2_en, r2_en, share, option, ps_logo, ljoy_press, rjoy_press = msg.buttons
+        ljoy_hort, ljoy_vert, l2, rjoy_hort, rjoy_vert, r2, dpad_hort, dpad_vert = (
+            msg.axes
+        )
+        (
+            cross,
+            circle,
+            triangle,
+            square,
+            l1,
+            r1,
+            l2_en,
+            r2_en,
+            share,
+            option,
+            ps_logo,
+            ljoy_press,
+            rjoy_press,
+        ) = msg.buttons
         x = y = omega = 0
         # set params before movement
-        #High Trigger toggle
+        # High Trigger toggle
         if option != self.prevOption:
             self.prevOption = option
             if option == 1:
@@ -69,35 +88,35 @@ Cross: Help Menu
                 self.turn *= self.scaling_factor.get(-dpad_hort, 1)
                 self.speed *= self.scaling_factor.get(dpad_vert, 1)
                 self.time = time.time()
-        if self.ena:     #Motors enabled and deadman engaged
-            #Convert motion into Twist() args
-            x = ljoy_vert/2 + rjoy_vert/2   #divide by 2 to add up to max of 1
-            y = rjoy_hort/2
+        if self.ena:  # Motors enabled and deadman engaged
+            # Convert motion into Twist() args
+            x = ljoy_vert / 2 + rjoy_vert / 2  # divide by 2 to add up to max of 1
+            y = rjoy_hort / 2
             omega = ljoy_hort * self.en_ljoy_hort
             if l2_en:
-                omega += (-(l2-1)/2)
-            if r2_en: 
-                omega += ((r2-1)/2)
-            omega = min(max(omega, -1),1)       #ensure within -1,1
-        #send out message
+                omega += -(l2 - 1) / 2
+            if r2_en:
+                omega += (r2 - 1) / 2
+            omega = min(max(omega, -1), 1)  # ensure within -1,1
+        # send out message
         cmd = Twist()
-        cmd.linear.x = self.speed*x
-        cmd.linear.y = self.speed*y
+        cmd.linear.x = self.speed * x
+        cmd.linear.y = self.speed * y
         cmd.linear.z = cmd.angular.x = cmd.angular.y = 0
         cmd.angular.z = self.turn * omega
         self.vel_pub.publish(cmd)
-        
+
         if cross:
             print(self.msg)
         print(self.status())
-        #self.debug(msg, cmd)
+        # self.debug(msg, cmd)
 
     def main(self):
         # enable the robot and enters velocity mode
         self.base_enable()
         rospy.spin()
         self.base_disable()
-    
+
     def debug(self, msg, cmd):
         print("Axes:" + str(msg.axes))
         print("Buttons:" + str(msg.buttons))
@@ -105,7 +124,7 @@ Cross: Help Menu
 
     def status(self):
         return f"Motors enabled:{self.ena}\nLeft Joystick Turn Enabled:{self.en_ljoy_hort}\n{self.vels()}"
-    
+
     def vels(self):
         return f"Vel: speed {self.speed:.4f}\tturn {self.turn:.4f} "
 
@@ -118,7 +137,8 @@ Cross: Help Menu
     def shutdown(self):
         self.base_disable()
 
-if __name__=="__main__":
-    rospy.init_node('teleop_twist_joystick')
+
+if __name__ == "__main__":
+    rospy.init_node("teleop_twist_joystick")
     js = joystickTeleop()
     js.main()
--- pcv_base/scripts/teleop_twist_keyboard.py
+++ pcv_base/scripts/teleop_twist_keyboard.py
@@ -6,14 +6,13 @@
 
 from __future__ import print_function
 
-#import roslib; roslib.load_manifest('teleop_twist_keyboard')
+# import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
-import os
-
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Byte
 
 import sys, select, termios, tty
+
 
 class keyboardTwistTeleop:
     def __init__(self):
@@ -46,39 +45,39 @@ class keyboardTwistTeleop:
         """
 
         self.moveBindings = {
-                'i':(1,0,0,0),
-                'o':(1,0,0,-1),
-                'j':(0,0,0,1),
-                'l':(0,0,0,-1),
-                'u':(1,0,0,1),
-                ',':(-1,0,0,0),
-                '.':(-1,0,0,1),
-                'm':(-1,0,0,-1),
-                'O':(1,-1,0,0),
-                'I':(1,0,0,0),
-                'J':(0,1,0,0),
-                'L':(0,-1,0,0),
-                'U':(1,1,0,0),
-                '<':(-1,0,0,0),
-                '>':(-1,-1,0,0),
-                'M':(-1,1,0,0),
-                't':(0,0,1,0),
-                'b':(0,0,-1,0),
-            }
+            "i": (1, 0, 0, 0),
+            "o": (1, 0, 0, -1),
+            "j": (0, 0, 0, 1),
+            "l": (0, 0, 0, -1),
+            "u": (1, 0, 0, 1),
+            ",": (-1, 0, 0, 0),
+            ".": (-1, 0, 0, 1),
+            "m": (-1, 0, 0, -1),
+            "O": (1, -1, 0, 0),
+            "I": (1, 0, 0, 0),
+            "J": (0, 1, 0, 0),
+            "L": (0, -1, 0, 0),
+            "U": (1, 1, 0, 0),
+            "<": (-1, 0, 0, 0),
+            ">": (-1, -1, 0, 0),
+            "M": (-1, 1, 0, 0),
+            "t": (0, 0, 1, 0),
+            "b": (0, 0, -1, 0),
+        }
 
-        self.speedBindings={
-                'q':(1.1,1.1),
-                'z':(.9,.9),
-                'w':(1.1,1),
-                'x':(.9,1),
-                'e':(1,1.1),
-                'c':(1,.9),
-            }
-        
-        velCmdTopic = rospy.get_param('velocity_command_topic', 'cmd_vel')
-        enaCmdTopic = rospy.get_param('control_mode_topic', 'control_mode')
-        self.pub = rospy.Publisher(velCmdTopic, Twist, queue_size = 1)
-        self.pub2 = rospy.Publisher(enaCmdTopic, Byte, queue_size = 1)
+        self.speedBindings = {
+            "q": (1.1, 1.1),
+            "z": (0.9, 0.9),
+            "w": (1.1, 1),
+            "x": (0.9, 1),
+            "e": (1, 1.1),
+            "c": (1, 0.9),
+        }
+
+        velCmdTopic = rospy.get_param("velocity_command_topic", "cmd_vel")
+        enaCmdTopic = rospy.get_param("control_mode_topic", "control_mode")
+        self.pub = rospy.Publisher(velCmdTopic, Twist, queue_size=1)
+        self.pub2 = rospy.Publisher(enaCmdTopic, Byte, queue_size=1)
         self.speed = rospy.get_param("~speed", 0.5)
         self.turn = rospy.get_param("~turn", 1.0)
         rospy.on_shutdown(self.shutdown)
@@ -96,12 +95,12 @@ class keyboardTwistTeleop:
     def getKey(self):
         tty.setraw(sys.stdin.fileno())
         res, _, _ = select.select([sys.stdin], [], [], 1.0)
-        key = '' if not res else sys.stdin.read(1)
+        key = "" if not res else sys.stdin.read(1)
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
         return key
 
     def vels(self, speed, turn):
-        return "currently:\tspeed %s\tturn %s " % (speed,turn)
+        return "currently:\tspeed %s\tturn %s " % (speed, turn)
 
     def run(self):
         x = 0
@@ -115,7 +114,7 @@ class keyboardTwistTeleop:
                 print(self.vels(self.speed, self.turn))
                 self.pub2.publish(Byte(data=1))
             while not rospy.is_shutdown():
-                key = self.getKey() # blocks here
+                key = self.getKey()  # blocks here
                 if key in self.moveBindings.keys():
                     x = self.moveBindings[key][0]
                     y = self.moveBindings[key][1]
@@ -125,7 +124,7 @@ class keyboardTwistTeleop:
                     self.speed *= self.speedBindings[key][0]
                     self.turn *= self.speedBindings[key][1]
                     print(self.vels(self.speed, self.turn))
-                    if (status == 14):
+                    if status == 14:
                         print(self.msg)
                     status = (status + 1) % 15
                 elif not key:
@@ -135,15 +134,19 @@ class keyboardTwistTeleop:
                     y = 0
                     z = 0
                     th = 0
-                    if (key == '\x70'):
+                    if key == "\x70":
                         self.pub2.publish(Byte(data=0))
-                    elif (key == '\x50'):
+                    elif key == "\x50":
                         self.pub2.publish(Byte(data=1))
-                    elif (key == '\x03'):
+                    elif key == "\x03":
                         break
                 twist = Twist()
-                twist.linear.x = x*self.speed; twist.linear.y = y*self.speed; twist.linear.z = z*self.speed
-                twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*self.turn
+                twist.linear.x = x * self.speed
+                twist.linear.y = y * self.speed
+                twist.linear.z = z * self.speed
+                twist.angular.x = 0
+                twist.angular.y = 0
+                twist.angular.z = th * self.turn
                 self.pub.publish(twist)
             self.pub2.publish(Byte(data=0))
         except Exception as e:
@@ -152,13 +155,17 @@ class keyboardTwistTeleop:
     def shutdown(self):
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
         twist = Twist()
-        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
         self.pub.publish(twist)
         self.pub2.publish(Byte(data=0))
-        
 
-if __name__=="__main__":
-    rospy.init_node('teleop_twist_keyboard')
+
+if __name__ == "__main__":
+    rospy.init_node("teleop_twist_keyboard")
     kb = keyboardTwistTeleop()
     kb.run()
